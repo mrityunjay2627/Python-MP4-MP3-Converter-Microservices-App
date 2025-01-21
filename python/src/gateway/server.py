386c5521fca95584242bbbbb51799d1a1c8daa49
Priyanshu M Sharma <mrityunjay2627@gmail.com>
2025-01-21 16:33:03 -0700

    Completed MongoDB and Notification Service Code

--- python/src/gateway/server.py
+++ python/src/gateway/server.py
@@ -1,16 +1,26 @@
 import os, json, pika, gridfs
-from flask import Flask, request
+from flask import Flask, request, send_file # send_file sends the file to the user
 from flask_pymongo import PyMongo
 from auth_svc import access
 from auth import validate
 from storage import util
+from bson.objectid import ObjectId
 
 server = Flask(__name__)
-server.config["MONGO_URI"] = "mongodb://host.minikube.internal:27017/videos"
+# server.config["MONGO_URI"] = "mongodb://host.minikube.internal:27017/videos"
 
-mongo = PyMongo(server)    
+mongo_video = PyMongo(
+    server,
+    uri="mongodb://host.minikube.internal:27017/videos"
+)    
 
-fs = gridfs.GridFS(mongo.db)
+mongo_mp3 = PyMongo(
+    server,
+    uri="mongodb://host.minikube.internal:27017/mp3s"
+) 
+
+fs_videos = gridfs.GridFS(mongo_video.db)
+fs_mp3s = gridfs.GridFS(mongo_mp3.db)
 
 connection = pika.BlockingConnection(pika.ConnectionParameters("rabbitmq"))
 channel = connection.channel()
@@ -31,6 +41,9 @@ def login():
 def upload():
     access, err = validate.token(request)
 
+    if err:
+        return err
+
     access = json.loads(access) # JSoN to Dictionary
 
     if access["admin"]:
@@ -40,7 +53,7 @@ def upload():
         for _, f in request.files.items():
             # _ represnts a key for the file
             # f represents the actual file as a value
-            err = util.upload(f, fs, channel, access)
+            err = util.upload(f, fs_videos, channel, access)
 
             if err:
                 return err
@@ -52,7 +65,30 @@ def upload():
 
 @server.route('/download', methods=["GET"])
 def download():
-    pass
+    access, err = validate.token(request)
+
+    if err:
+        return err
+
+    access = json.loads(access) # JSoN to Dictionary
+
+    if access["admin"]:
+        fid_string = request.args.get("fid")
+
+        if not fid_string:
+            return "fid is required",400
+
+        try:
+            out = fs_mp3s.get(ObjectId(fid_string))
+            return send_file(out, download_name=f'{fid_string}.mp3')
+        except Exception as err:
+            print(err)
+            return "internal server error", 500
+
+    
+    return "not authorized", 401
+
+
 
 if __name__ == "__main__":
     server.run(host="0.0.0.0", port=8080)
